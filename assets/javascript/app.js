a9f0ea40bfdd2e07e75ebbea9a49f95ac18c8e73
violet chang <violetchang@violets-MacBook-Pro.local>
2019-05-28 23:01:13 -0500

    updated javascript to show all questions and answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -58,6 +58,7 @@ $("#startClock").on("click", function run() {
         $('#startButton').hide();
         $('#results').hide();
         $('#submitForm').show();
+        $('#questionDiv').show();
 
 
         clearInterval(intervalId);
@@ -81,88 +82,28 @@ $("#startClock").on("click", function run() {
                 $('#questionDiv').hide();
                 $('#countdownTimer').hide();
                 $('.lead').hide();
-                $("#results").html("Correct Answers: "+correctAnswers+"<br><br>Incorrect Answers: "+inCorrectAnswers+"<br><br>Unanswered: "+unAnswers);
+                $("#results").html("Correct Answers: " + correctAnswers + "<br><br>Incorrect Answers: " + inCorrectAnswers + "<br><br>Unanswered: " + unAnswers);
+
+                //if radioBtn+index === questionsAnswers.rightAnswer, then correctAnswers++
+                //else, incorrectAnswers++
+                //unanswers = questionAnswers.length - (correctAnswers+incorrectAnswers)
             }
         }
 
 
-//q1
-        var newDiv = $("<div><br>");
-        var showQuestions = $("<p>")
-        showQuestions.html("<strong>" + questionAnswers[0].question + "</strong>")
-        $("#questionDiv").append(showQuestions);
-
-        newDiv = $("<div>");
-        showPossibleAnswers = $("<div>")
-        $("#questionDiv").append("<input class='response' type='radio' data-right=" + questionAnswers[0].rightAnswer + " name='answer1'>" + questionAnswers[0].answers[0] + "&nbsp;&nbsp;&nbsp;&nbsp;");
-        $("#questionDiv").append("<input class='response' type='radio' name='answer1'>" + questionAnswers[0].answers[1] + "&nbsp;&nbsp;&nbsp;&nbsp;");
-        $("#questionDiv").append("<input class='response' type='radio' name='answer1'>" + questionAnswers[0].answers[2] + "&nbsp;&nbsp;&nbsp;&nbsp;<br><br><br>");
-
-
-//q2
-        var newDiv = $("<div><br>");
-        var showQuestions = $("<p>")
-        showQuestions.html("<strong>" + questionAnswers[1].question)
-        $("#questionDiv").append(showQuestions);
-
-        newDiv = $("<div>");
-        var showPossibleAnswers = $("<div>")
-        $("#questionDiv").append("<input class='response' type='radio' name='answer2'>" + questionAnswers[1].answers[0] + "&nbsp;&nbsp;&nbsp;&nbsp;");
-        $("#questionDiv").append("<input class='response' type='radio' name='answer2'>" + questionAnswers[1].answers[1] + "&nbsp;&nbsp;&nbsp;&nbsp;");
-        $("#questionDiv").append("<input class='response' type='radio' name='answer2'>" + questionAnswers[1].answers[2] + "&nbsp;&nbsp;&nbsp;&nbsp;<br><br><br>");
-
-
-//q3
-        var newDiv = $("<div>");
-        var showQuestions = $("<p>")
-        showQuestions.html("<strong>" + questionAnswers[2].question)
-        $("#questionDiv").append(showQuestions);
-
-        newDiv = $("<div>");
-        var showPossibleAnswers = $("<div>")
-        $("#questionDiv").append("<input class='response' type='radio' name='answer3'>" + questionAnswers[2].answers[0] + "&nbsp;&nbsp;&nbsp;&nbsp;");
-        $("#questionDiv").append("<input class='response' type='radio' name='answer3'>" + questionAnswers[2].answers[1] + "&nbsp;&nbsp;&nbsp;&nbsp;");
-        $("#questionDiv").append("<input class='response' type='radio' name='answer3'>" + questionAnswers[2].answers[2] + "&nbsp;&nbsp;&nbsp;&nbsp;<br><br><br>");
-
-
-//q4
-        var newDiv = $("<div>");
-        var showQuestions = $("<p>")
-        showQuestions.html("<strong>" + questionAnswers[3].question)
-        $("#questionDiv").append(showQuestions);
-
-        newDiv = $("<div>");
-        var showPossibleAnswers = $("<div>")
-        $("#questionDiv").append("<input class='response' type='radio' name='answer4'>" + questionAnswers[3].answers[0] + "&nbsp;&nbsp;&nbsp;&nbsp;");
-        $("#questionDiv").append("<input class='response' type='radio' name='answer4'>" + questionAnswers[3].answers[1] + "&nbsp;&nbsp;&nbsp;&nbsp;");
-        $("#questionDiv").append("<input class='response' type='radio' name='answer4'>" + questionAnswers[3].answers[2] + "&nbsp;&nbsp;&nbsp;&nbsp;<br><br><br>");
-
-
-//q5
-        var newDiv = $("<div>");
-        var showQuestions = $("<p>")
-        showQuestions.html("<strong>" + questionAnswers[4].question)
-        $("#questionDiv").append(showQuestions);
-
-        newDiv = $("<div>");
-        var showPossibleAnswers = $("<div>")
-        $("#questionDiv").append("<input class='response' type='radio' name='answer5'>" + questionAnswers[4].answers[0] + "&nbsp;&nbsp;&nbsp;&nbsp;");
-        $("#questionDiv").append("<input class='response' type='radio' name='answer5'>" + questionAnswers[4].answers[1] + "&nbsp;&nbsp;&nbsp;&nbsp;");
-        $("#questionDiv").append("<input class='response' type='radio' name='answer5'>" + questionAnswers[4].answers[2] + "&nbsp;&nbsp;&nbsp;&nbsp;<br><br><br>");
-
-
+    
         //loop through each trivia question, make each answer into a radio button
-        //         function showTrivia(){
-        //             $("#showQuestion").html(questionAnswers[index].question);
-        //             for(var i=0; i< questionAnswers[index].answers.length;i++){
-        //               $("#showPossibleAnswers").append("<br><input class='response' type='radio' name=" + i + " value=" + i + ">" + questionAnswers[index].answers[i] +"&nbsp;&nbsp;&nbsp;&nbsp;")
+        function showTrivia() {
+            for (index=0; index < questionAnswers.length; index++){            
+                $("#questionDiv").append(questionAnswers[index].question);
+                for (var i = 0; i < questionAnswers[index].answers.length; i++) {
+                    $("#questionDiv").append("<br><input class='response' type='radio' name='radioBtn"+index+"' value=" + i + ">" + questionAnswers[index].answers[i])
+                }
+                $("#questionDiv").append("<br><br>");
+            }
 
-        //                       //if user input and answer are equal, user gets a point.
-
-        // }
-
-        //           }
-        //           showTrivia();
+        }
+        showTrivia();
 
 
     }
